7b359e98403a31e4d5747e7e7a145f3004565931
the-nerdy-geek <57044943+the-nerdy-geek@users.noreply.github.com>
2023-11-28 15:41:05 -0500

    Update ch3_statecapitals4.py

--- Chapter03/ch3_statecapitals4.py
+++ Chapter03/ch3_statecapitals4.py
@@ -5,7 +5,7 @@ state_capitals = {
     }
 state_capitals["Iowa"] = "Des Moines"
 
-state = raw_input("What state's capital are you looking for today? ")
+state = input("What state's capital are you looking for today? ")
 capital = state_capitals[state]
 
 
