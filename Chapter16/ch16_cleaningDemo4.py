5655898b39f9698a1acd3e864d05dc2482b9e88b
dayrenem <74788561+dayrenem@users.noreply.github.com>
2023-12-18 20:27:40 -0500

    Update ch16_cleaningDemo4.py

--- Chapter16/ch16_cleaningDemo4.py
+++ Chapter16/ch16_cleaningDemo4.py
@@ -3,8 +3,8 @@ import pandas as pd
 data = {
 'Title': ['The Midnight Library', 'The Forever War', 'The Power',
 'The Midnight Library', 'The Forever War', 'To Kill a Mockingbird'],
-'Author': ['Matt Haig', 'Joe Haledeman', 'Naomi Alderman', 'Matt
-Haig', 'Joe Haledeman', 'Harper Lee']
+'Author': ['Matt Haig', 'Joe Haledeman', 'Naomi Alderman', 'Matt Haig', 
+           'Joe Haledeman', 'Harper Lee']
 }
 
 df = pd.DataFrame(data)
