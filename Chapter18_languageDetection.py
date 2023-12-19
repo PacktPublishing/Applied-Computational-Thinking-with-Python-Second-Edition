fe462ed3b2fb11561a1a1c46b7c8851626dc74af
dayrenem <74788561+dayrenem@users.noreply.github.com>
2023-12-18 23:32:22 -0500

    Create Chapter18_languageDetection.py

---
+++ Chapter18_languageDetection.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Load your dataset
+dataset_path = r'C:\Users\PATH_TO_FILE\dataset.csv'
+df = pd.read_csv(dataset_path)
+X = df['Text']
+y = df['language']
+
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a pipeline
+pipeline = Pipeline([
+('vectorizer', CountVectorizer()),
+('classifier', MultinomialNB())
+])
+
+# Train the model
+pipeline.fit(X_train, y_train)
+
+# Predictions
+y_pred = pipeline.predict(X_test)
+
+# Evaluate the model
+print(classification_report(y_test, y_pred))
+conf_mat = confusion_matrix(y_test, y_pred)
+
+# Plot confusion matrix
+plt.figure(figsize=(10,10))
+plt.imshow(conf_mat, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.colorbar()
+tick_marks = np.arange(len(y.unique()))
+plt.xticks(tick_marks, y.unique(), rotation=45)
+plt.yticks(tick_marks, y.unique())
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
