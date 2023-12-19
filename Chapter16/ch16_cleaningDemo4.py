8ab69ecbaf771e642e6682ab75d7b46f0759ca0f
dayrenem <74788561+dayrenem@users.noreply.github.com>
2023-12-18 20:25:37 -0500

    Create ch16_cleaningDemo4.py

---
+++ Chapter16/ch16_cleaningDemo4.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+'Title': ['The Midnight Library', 'The Forever War', 'The Power',
+'The Midnight Library', 'The Forever War', 'To Kill a Mockingbird'],
+'Author': ['Matt Haig', 'Joe Haledeman', 'Naomi Alderman', 'Matt
+Haig', 'Joe Haledeman', 'Harper Lee']
+}
+
+df = pd.DataFrame(data)
+
+print("Original Dataset:\n")
+
+print(df) 
